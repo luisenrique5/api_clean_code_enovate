cbd4faef30a2f19ea1980b873c2faea9647ea849
Luis Enrique <supelanosuarezluisenrique5@gmail.com>
2024-08-09 13:58:14 -0500

    Refactor project structure and remove unnecessary files

--- src/config.py
+++ src/config.py
@@ -1,14 +1,25 @@
 import os
 from dotenv import load_dotenv
 from pymongo import MongoClient
+import json
+from os.path import join, dirname
 
-load_dotenv()
+dotenv_path = join(dirname(__file__), '../env/.env')
+load_dotenv(dotenv_path)
+
+config_json_path = join(dirname(__file__), '../config/config.json')
+with open(config_json_path, 'r') as f:
+    config_data = json.load(f)
 
 class Config:
     MONGO_URI = os.getenv('MONGO_URI')
     EXTERNAL_API = os.getenv('EXTERNAL_API')
     EXTERNAL_API_TOKEN = os.getenv('EXTERNAL_API_TOKEN')
     INTERNAL_API_TOKEN = os.getenv('INTERNAL_API_TOKEN')
+    ENVIRONMENT = config_data["ENVIRONMENT"]
+    LOGGINGLEVEL = config_data["LOGGINGLEVEL"]
+    REMOTEDOCKERIMAGE = config_data["REMOTEDOCKERIMAGE"]
+    KEYPATH = config_data["KEYPATH"]
 
     @staticmethod
     def get_mongo_collection(db_name='prodanalytics', collection_name='monthlyaverage'):
